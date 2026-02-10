1acc2dd52de4f21c6574c876192021464add2ced
Vaishnavi dandekar <vaishnavidandekar81@gmail.com>
2026-02-10 13:06:01 +0530

    fixed correct settings file

--- job_vacancy/build.sh
+++
@@ -1,8 +0,0 @@
-touch build.sh
-chmod +x build.sh
-#!/usr/bin/env bash
-set -o errexit
-
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
--- job_vacancy/settings.py
+++ job_vacancy/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
 
 # Application definition
