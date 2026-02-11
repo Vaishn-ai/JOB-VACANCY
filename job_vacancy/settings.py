f14e6c127e1ef3f92ae27fb2c2712f2cf93429c5
Vaishnavi dandekar <vaishnavidandekar81@gmail.com>
2026-02-11 11:15:40 +0530

    auto create user

--- job_vacancy/settings.py
+++ job_vacancy/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
@@ -129,3 +129,5 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
--- portal/apps.py
+++ portal/apps.py
@@ -3,3 +3,10 @@ from django.apps import AppConfig
 
 class PortalConfig(AppConfig):
     name = 'portal'
+
+class PortalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'portal'
+
+    def ready(self):
+        import portal.signals
